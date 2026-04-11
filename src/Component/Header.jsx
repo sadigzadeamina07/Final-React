93568654d7ca39e4cba542ef4646a45bcbc829a4
sadigzadeamina07 <sadigzadeamina7@gmail.com>
2026-04-11 23:29:52 +0400

    second

---
+++ src/Component/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <div className="bg-[#fde8e0] py-2">
+             <div className="text-center text-[#340c0c] text-sm ">
+    <p>Create an account or log in to unlock 15% off + FREE ground shipping on your first order* with code DARLING15</p>
+    <p>Unlock A Free Mini Skincare Duo When You Spend $90! T&Cs Apply.</p>
+    <p>Create an account or log in to unlock 15% off + FREE ground shipping on your first order* with code DARLING15</p>
+    <p>
+      
+    </p>
+    </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
