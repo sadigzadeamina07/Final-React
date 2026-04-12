06205cf9eee1890d363c7d5e34627227b80ee35e
sadigzadeamina07 <Sadigzadeamina7@gmail.com>
2026-04-12 15:05:29 +0400

    added state

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Header() {
+  const message=[
+    "Create an account or log in to unlock 15% off + FREE ground shipping on your first order* with code DARLING15",
+    "Unlock A Free Mini Skincare Duo When You Spend $90! T&Cs Apply."
+  ]
+  const [index,setIndex]=useState(0)
+const [fade,setFade]=useState(false)
   return (
     <div>
         <div className="bg-[#fde8e0] py-2">
              <div className="text-center text-[#340c0c] text-sm ">
-    <p>Create an account or log in to unlock 15% off + FREE ground shipping on your first order* with code DARLING15</p>
-    <p>Unlock A Free Mini Skincare Duo When You Spend $90! T&Cs Apply.</p>
-    <p>Create an account or log in to unlock 15% off + FREE ground shipping on your first order* with code DARLING15</p>
-    <p>
-      
-    </p>
+<a href="" className='fade ? opacity-0 : opacity-1'>{message[index]} </a>
     </div>
         </div>
     </div>
